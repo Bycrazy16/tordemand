ac70ffe3ed927819c87316f83109d6495c55bb97
Bycrazy16 <erinmusaj@proton.me>
2025-11-11 15:23:42 +0100

    able to query

--- server.js
+++ server.js
@@ -1,19 +1,44 @@
 const express = require('express');
+const fs = require('fs');
 const path = require('path');
-require('dotenv').config();
+const games = require('./sites/games');
+const env = require("./.env")
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = env.PORT | 6969;
 
-// Serve static files (HTML, CSS, JS)
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Homepage
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'search.html'));
+app.get('/results', async (req, res) => {
+  const query = req.query.q;
+  const type = req.query.type || 'game'; // "game" o "movie"
+
+  if (!query) return res.send('Inserisci un termine di ricerca');
+
+  let results = [];
+  try {
+    if (type === 'game') {
+      results = await games.searchAll(query);
+    } else if (type === 'movie') {
+      // results = await films.searchAll(query);
+    }
+  } catch (err) {
+    console.error(err);
+    return res.send('Errore nella ricerca');
+  }
+
+  // Genera HTML dei risultati
+  const resultsHtml = results.map(item => {
+    const links = item.links.map(link => `<div class="links"><a href="${link}" target="_blank">${link}</a></div>`).join('');
+    return `<div class="result"><div class="title">${item.title}</div>${links}</div>`;
+  }).join('');
+
+  const template = fs.readFileSync(path.join(__dirname, './views/results.html'), 'utf8');
+  const html = template.replace('{{query}}', query).replace('{{results}}', resultsHtml);
+
+  res.send(html);
 });
 
-// Start server
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+
+
+app.listen(PORT, () => console.log(`Server avviato su http://localhost:${PORT}`));
